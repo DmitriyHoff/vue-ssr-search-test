d944b4c8437fa45042dda3fb533ebd5ee9866d18
Dmitriy Goff <goffdmitriy@gmail.com>
2024-12-08 20:37:55 +0200

    add active color selector

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import type { OpenStreetMapObject } from '@/typings/interface';
+
 defineProps<{
     resultObject: OpenStreetMapObject
+    active?: boolean
 }>()
 </script>
 
 <template>
-    <div class="result">
+    <div class="result" :class="{ 'result--active': active }">
         <span class="result__display-name">{{ resultObject.display_name }}</span>
         <span class="result__type capitalize-first">{{ resultObject.type }}</span>
     </div>
@@ -24,11 +26,15 @@ defineProps<{
         font-size: 0.75rem;
         color: var(--color-green);
     }
+
+    &--active {
+        background-color: var(--color-green-active);
+    }
 }
 
 @media (hover: hover) {
     .result:hover {
-        background-color: hsla(160, 100%, 37%, 0.2);
+        background-color: var(--color-green-hover);
     }
 }
 </style>

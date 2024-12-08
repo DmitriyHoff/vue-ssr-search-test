6a2824ed3205c32477f5b3247da6ec4907422de0
Dmitriy Goff <goffdmitriy@gmail.com>
2024-12-08 20:34:59 +0200

    add notations

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -6,9 +6,11 @@ import { useSearchStore } from '@/stores/search';
 import { storeToRefs } from 'pinia';
 import type { Map } from 'leaflet';
 
+/** Шаблон URL для загрузки изображений карты */
+const TILE_LAYER = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+
 /* Leaflet работет только в брауре поэтому пока достаточно знать
- * только тип импортируемого модуля
- */
+ * только тип импортируемого модуля */
 type LefletModule = typeof import("leaflet");
 
 const props = defineProps<{
@@ -19,41 +21,46 @@ const props = defineProps<{
 
 const searchStore = useSearchStore();
 const { selectedResult } = storeToRefs(searchStore)
-const TILE_LAYER = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+
 const isMounted = ref<boolean>(false);
 let leafletModule: LefletModule
 let mapObject: Map;
 
-watch(() => selectedResult.value, (newValue) => {
+/** При изменении выбранного результата поиска следует обновить карту */
+watch(selectedResult, (newValue) => {
     if (!isMounted.value) return
+
     const { boundingbox, lat, lon } = newValue || {}
-    if (boundingbox) {
-        const corner1 = leafletModule.latLng(boundingbox[0], boundingbox[2])
-        const corner2 = leafletModule.latLng(boundingbox[1], boundingbox[3])
-        const bounds = leafletModule.latLngBounds(corner1, corner2);
 
-        if (lat && lon) {
-            leafletModule.tileLayer(TILE_LAYER).addTo(mapObject);
-            mapObject.flyToBounds(bounds, {
-                animate: true,
-                duration: 1.2
-            })
-        }
-    }
-}, { deep: true })
+    if (!boundingbox) return
+    if (!lat || !lon) return
 
-/** Событие onMounted отработает только в браузере,
+    const corner1 = leafletModule.latLng(boundingbox[0], boundingbox[2])
+    const corner2 = leafletModule.latLng(boundingbox[1], boundingbox[3])
+    const bounds = leafletModule.latLngBounds(corner1, corner2);
+
+    leafletModule.tileLayer(TILE_LAYER).addTo(mapObject);
+    mapObject.flyToBounds(bounds, {
+        animate: true,
+        duration: 1.2
+    })
+})
+
+/* Событие onMounted отработает только в браузере,
  * а это значит, что доступен объект window, который необходим
  * для работы библиотеки leaflet
  */
 onMounted(async () => {
-    console.log("Map mounted %o", props.bbox)
     isMounted.value = true
 
     // импортируем leaflet и сохраняем модуль
     const { default: leaflet } = await import('leaflet')
     leafletModule = leaflet
+
+    // инициализирем объект карты
     mapObject = leaflet.map('map', { attributionControl: false })
+
+    // Устанавливаем точку центра и масштаб для первой отрисовки
     mapObject.setView([props.marker[0], props.marker[1]], 13);
     leaflet.tileLayer(TILE_LAYER).addTo(mapObject);
 })
